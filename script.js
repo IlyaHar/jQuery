6a6f9af61369d421402073f5e2ca3e0e68c2fda8
Ilya <harcenkoila278@gmail.com>
2023-05-03 10:41:28 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    //Завдання 1
+    $("#addHtml").html("<h1>New Element</h1>");
+
+    //Завдання 2
+    $("div.text").hide();
+
+    //Завдання 3
+    $("section").remove();
+
+    //Завдання 4
+    $("#div").addClass("addClass").text("Lumos Maxima");
+});
+
